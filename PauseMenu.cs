a7298886a44746f5aa662d757f8f2834572ff5a1
Banana Oscura <190346331+bananaoscura@users.noreply.github.com>
2024-12-12 03:59:29 -0300

    menú de pausa

---
+++ PauseMenu.cs
@@ -0,0 +1,54 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // botón pausa
+    private void OnPause()
+    {
+        if (GameIsPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+        //Resume();
+        GameIsPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
+
+
